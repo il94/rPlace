1a4d570cdb0c19193da51d1d3e392d9ed86d8d7c
il94 <ilyes.landolsi@outlook.fr>
2024-04-27 01:01:58 +0200

    feat: add animated background app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,35 @@
 import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home"
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import background from "./utils/background";
 
-function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</Router>
+const App = () => {
+    const [ init, setInit ] = useState(false)
+
+    // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            await loadFull(engine)
+        }).then(() => {
+            setInit(true)
+        });
+    }, []);
+
+    return (
+		<>
+			{ init && <Particles id="tsparticles" options={background} /> }
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
+		</>
 	)
 }
 
